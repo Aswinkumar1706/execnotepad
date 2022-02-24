de56a918f079612a6e90426a03a09d08e947a300
Aswinkumar1706 <kaswinsurya1706@gmail.com>
2022-02-24 11:23:24 +0530

    second changes

--- calexec.php
+++ calexec.php
@@ -1,6 +1,8 @@
 <?php
 function php_func(){
-    shell_exec('start Notepad');
+    //shell_exec('start Notepad');
+	$WshShell = new COM("WScript.Shell");
+$oExec = $WshShell->Run("notepad.exe", 7, false);
 }
 php_func();
 ?>
